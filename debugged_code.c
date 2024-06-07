4b722f2e49295df3030926afe9ea0261ed1d3981
Noemi <166412931+adaagioo@users.noreply.github.com>
2024-06-07 18:25:00 +0200

    Update debugged_code.c

--- debugged_code.c
+++ debugged_code.c
@@ -6,11 +6,11 @@ void dividi();
 void ins_string();
 
 int main() {
-    char scelta;
+    char scelta; //variabile per memorizzare la scelta dell'utente
     do {
-        menu();
+        menu(); //mostra il menù delle opzioni
         printf("Inserisci qui la tua scelta: ");
-        scanf(" %c", &scelta); 
+        scanf(" %c", &scelta); //legge la scelta dell'utente
         switch (scelta) {
             case 'A':
             case 'a':
@@ -47,17 +47,17 @@ int main() {
     return 0;
 }
 
-void menu() {
+void menu() { // Funzione che mostra il menu
     printf("Benvenuto, sono un assistente digitale, posso aiutarti a sbrigare alcuni compiti\n");
     printf("Come posso aiutarti?\n");
     printf("A >> Moltiplicare due numeri\nB >> Dividere due numeri\nC >> Inserire una stringa\nD >> Uscire\n");
 }
-
+// Funzione per moltiplicare due numeri
 void moltiplica() {
     int a, b;
     printf("Inserisci il primo numero: ");
-    while (scanf("%d", &a) != 1) {
-        while (getchar() != '\n'); 
+    while (scanf("%d", &a) != 1) {     // Controlla che l'input sia valido
+        while (getchar() != '\n'); // Pulisce il buffer
         printf("Per favore inserisci un numero valido: ");
     }
     printf("Inserisci il secondo numero: ");
@@ -69,7 +69,7 @@ void moltiplica() {
     int prodotto = a * b;
     printf("Il prodotto tra %d e %d è: %d\n", a, b, prodotto);
 }
-
+// Funzione per dividere due numeri
 void dividi() {
     float a, b;
     printf("Inserisci il numeratore: ");
@@ -90,11 +90,11 @@ void dividi() {
         printf("Impossibile dividere per zero.\n");
     }
 }
-
+// Funzione per inserire una stringa
 void ins_string() {
-    char stringa[11];
-    printf("Inserisci una stringa di massimo 10 caratteri: ");
-    scanf(" %10[^\n]", stringa);
+    char stringa[11]; // Buffer per la stringa con massimo 10 caratteri
+    printf("Inserisci una stringa di massimo 10 caratteri: "); 
+    scanf(" %10[^\n]", stringa);// Legge una stringa di massimo 10 caratteri
     printf("La stringa inserita è: %s\n", stringa);
 }
 
